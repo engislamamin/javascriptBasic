f018ac9c74ba189258654f4973d4a1a822a1e637
engislamamin <eng.islam.201@gmail.com>
2025-05-28 19:19:57 +0300

    update

--- 2.functionProgramming/js1_function.js
+++ 2.functionProgramming/js1_function.js
@@ -13,9 +13,10 @@ funcWithoutPara();
 //Function with parameters.
 
 function funcWithPara(name, age, address) {
-    // Look at here are two way to print the parameters or variables.
-    console.log("function without parameter " + "\n    Name is " + name + "\n    Age is " + age + "\n    address is " + address);
-     console.log(`Function with parameters:
+    // Two ways to print variables: string concatenation and template literals.
+
+    console.log("Function with parameters:\n    Name is " + name + "\n    Age is " + age + "\n    Address is " + address);
+    console.log(`Function with parameters:
         Name is ${name}
         Age is ${age}
         Address is ${address}`);
