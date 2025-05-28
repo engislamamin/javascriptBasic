6824fce6774ecaaf2d56033b2fe8325fbc73cf6e
engislamamin <eng.islam.201@gmail.com>
2025-05-28 19:11:18 +0300

    update

--- 2.functionProgramming/js1_function.js
+++ 2.functionProgramming/js1_function.js
@@ -2,13 +2,18 @@
 
 
 /* function */
+//function without parameter
 
-function funcWithoutPara() { /*--------------> function without parameter */
+function funcWithoutPara() { 
     console.log("function without parameter");
  }
 funcWithoutPara();
-
-function funcWithPara(name, age, adders) { /*--------------> function without parameter */
-    console.log("function without parameter "+"\n    Name is "+name+"\n    Age is "+age+"\n    Adderss is "+adders);
+//function with parameter
+function funcWithPara(name, age, address) { /*--------------> */
+    console.log("function without parameter " + "\n    Name is " + name + "\n    Age is " + age + "\n    address is " + address);
+     console.log(`Function with parameters:
+        Name is ${name}
+        Age is ${age}
+        Address is ${address}`);
  }
 funcWithPara("Islam", 29, "Aswan");
